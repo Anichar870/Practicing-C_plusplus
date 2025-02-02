7967199db3abd9407bee2e08e4246c0495a7dc86
Anichar <ponglijamir870@gmail.com>
2025-02-02 09:25:28 +0530

    Add files via upload

---
+++ dynamic initialization array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[5];
+    
+    num[0]=12;
+    num[1]=67;
+    num[2]=65;
+    num[3]=18;
+    num[4]=9;
+    
+    for(int i=0; i<5; i++){
+        cout<< num[i]<< " ";
+    }
+    return 0;
+}
+    
