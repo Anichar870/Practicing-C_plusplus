60637910ee0f5552a14d4a5358bf5b96ecd491e2
Anichar <ponglijamir870@gmail.com>
2025-02-02 09:28:59 +0530

    Add files via upload

---
+++ Partial Initialization array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[7]= {1,2,3,4};
+    
+    for(int i=0; i<7 ; i++){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+}
+    
